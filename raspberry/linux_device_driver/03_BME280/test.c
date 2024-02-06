b7c722b8dc9a9fc8c4282a28f22bf0999d829703
toan.pham <thanhtoanpham36@gmail.com>
2024-02-06 22:04:15 +0700

    read BME280

--- raspberry/linux_device_driver/03_BME280/test.c
+++ raspberry/linux_device_driver/03_BME280/test.c
@@ -3,13 +3,18 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-int main() {
-	int dev = open("/dev/bme280", O_RDONLY);
-	if(dev == -1) {
-		printf("Opening was not possible!\n");
-		return -1;
-	}
-	printf("Opening was successfull!\n");
-	close(dev);
-	return 0;
+int main()
+{
+    int dev = open("/dev/bme280", O_RDONLY);
+    if (dev == -1)
+    {
+        printf("Opening was not possible!\n");
+        return -1;
+    }
+    printf("Opening was successfull!\n");
+    char buf[31];
+    read(dev, buf, 31);
+    printf("buf: %s", buf);
+    close(dev);
+    return 0;
 }
